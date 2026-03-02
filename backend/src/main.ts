f67a2de769e4953556dd667045e11bf89bfd04d2
LitJekko <tu.email@ejemplo.com>
2026-03-02 12:26:26 -0500

    cargar datos

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,8 +4,13 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  // ¡Nuestra llave mágica para Vercel!
   app.enableCors();
+
+  // Escuchar en el puerto de Railway (o 4000) y abrir la red con '0.0.0.0'
+  const port = process.env.PORT || 4000;
+  await app.listen(port, '0.0.0.0');
   
-  await app.listen(process.env.PORT ?? 4000);
+  console.log(`¡Backend del Hanma Gym corriendo en el puerto ${port}!`);
 }
 bootstrap();
